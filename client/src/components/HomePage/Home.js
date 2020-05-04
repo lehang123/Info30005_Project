467ed32d5387f1fd9aab289a808224fbfc3199c5
starsheeep1016 <61729008+starsheeep1016@users.noreply.github.com>
2020-05-04 17:26:04 +1000

    add info to homepage

--- client/src/components/HomePage/Home.js
+++ client/src/components/HomePage/Home.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
+import picGavi from '../../Gavi.png';
+import picOpenideo from '../../OpenIdeo.png';
+
 
 class Home extends Component{
     render(){
         return(
-            <h1>
-                Home
-            </h1>
+            <div>
+                <h1 class = 'textSet'> Information about our Team <br/><br/></h1>
+                <h4> IN COLLABORATION WITH: <br/><br/></h4>
+                <img class = 'collPic' src = {picGavi} alt = 'Gavi Icon' width="220" height="80"/>
+                <img class = 'collPic' src = {picOpenideo} alt = 'OpenIdeo Icon' width="250" height="70"/>
+                <h3> In response to the growing threat of pandemic disease </h3>
+                <h3> our team have taken up the challenge to provide solutions to those affected </h3>
+            </div>
         ); 
     }
 }
--- client/src/components/Navi.js
+++ client/src/components/Navi.js
@@ -19,10 +19,10 @@ function Navi() {
       </div>
       <ul className="Navi-links">
         <Link style={naviStyle} to= '/patients'><li>Patients</li></Link>
-        <Link style={naviStyle} to= '/faculties'><li>faculties</li></Link>
-        <Link style={naviStyle} to= '/vaccines'><li>vaccines</li></Link>
-        <Link style={naviStyle} to= '/aboutus'><li>about us</li></Link>
-        <Link style={naviStyle} to= '/login'><li>login</li></Link>
+        <Link style={naviStyle} to= '/faculties'><li>Faculties</li></Link>
+        <Link style={naviStyle} to= '/vaccines'><li>Vaccines</li></Link>
+        <Link style={naviStyle} to= '/aboutus'><li>AboutUs</li></Link>
+        <Link style={naviStyle} to= '/login'><li>Login</li></Link>
       </ul>
     </nav>
   );
