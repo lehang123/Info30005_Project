bff98d0c0ff4ed1514fe7bb6e6eb65ed87590b92
Anthony <anthony.singgih@yahoo.com>
2020-05-07 01:17:27 +1000

    commit

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -3,15 +3,15 @@ import React from 'react';
 function Login() {
   return(
       <body className="body">
-      <div>Sign In</div>
-      <div className="input-container">
-        <input type="text" placeholder="Username or Email" required=""/>
-      </div>
-      <div className="input-container">
-        <input type="text" placeholder="Password" required=""/>
-      </div>
-      <button>Next</button>
-      <div id="login-link">Forgot Password / Sign Up</div>
+        <div>Sign In</div>
+        <div className="input-container">
+            <input type="text" placeholder="Username or Email" required=""/>
+        </div>
+        <div className="input-container">
+            <input type="text" placeholder="Password" required=""/>
+        </div>
+        <button>Next</button>
+        <div id="login-link">Forgot Password / Sign Up</div>
       </body>
   )
 }
