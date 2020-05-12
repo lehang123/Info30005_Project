44320c26f961b952f6756a21c0dd542d0ecc26e3
Anthony <anthony.singgih@yahoo.com>
2020-05-12 15:01:08 +1000

    just a typo
    just a typo

--- client/src/components/vaccines/Vaccines.js
+++ client/src/components/vaccines/Vaccines.js
@@ -18,17 +18,16 @@ function Vaccines(props) {
   }
 
   return (
-    <div>
-      {items.map(item => 
+    <div className="Vaccines">
+      <div>{items.map(item =>
         (<ul key={item.id}>
           <li><Link to = {`/vaccines/${item.id}`}>name : {item.name}</Link></li>
           <li>prevent_disease : {item.prevent_disease}</li>
-          <li>alleries : {item.alleries}</li>
+          <li>allergies : {item.alleries}</li>
           <li>recommend_star : {item.recommend_star}</li>
         </ul>
-      ))}
+        ))}</div>
     </div>
-      
   );
 }
 
