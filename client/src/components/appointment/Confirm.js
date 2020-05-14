622e8519b703e9b63ee7e7d6a57e9a0f70fa350c
Linjing Xuan <linjingxuan@gmail.com>
2020-05-14 20:09:39 +1000

    appointment api

--- client/src/components/appointment/Confirm.js
+++ client/src/components/appointment/Confirm.js
@@ -7,15 +7,23 @@ import RaisedButton from 'material-ui/RaisedButton'
 export class Confirm extends Component {
     continue = e => {
         e.preventDefault();
-        const {values : {firstName, lastName, email, phone, address, vaccine, hospital, datetime, allergy, emergencyContactName, emergencyContactPhone, medicareNumber}} = this.props;
-        var data = {"vaccine": {vaccine}, "hospital": {hospital}, "datetime": {datetime}}
-        fetch("https://team-vaccine.herokuapp.com/appointments", {
+        const { values: { firstName, lastName, email, phone, address, vaccine, hospital, datetime, allergy, emergencyContactName, emergencyContactPhone, medicareNumber } } = this.props;
+        var data1 = { patient_id: "5eb916ceafa0227c0f91ce16", hospital_id: "5e85bf5520c0af944747c294", date_time: "2012-03-19T07:22Z", cost: 123, vaccine_id: "5e859b72932597849c779076" }
+        var url = 'http://localhost:5000/api/appointments'
+        if (process.env.NODE_ENV === 'production') {
+            url = '/api/appointments'
+        }
+        fetch(url, {
             method: "POST",
-            body: JSON.stringify(data)
-        }).then(function(response){
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data1)
+        }).then(function (response) {
             return response.json();
-        }).then(function(data){
-            console.log(data)
+        }).then(function (data1) {
+            console.log(JSON.stringify(data1))
         })
         this.props.nextStep();
     }
@@ -26,74 +34,74 @@ export class Confirm extends Component {
     }
 
     render() {
-        const {values : {firstName, lastName, email, phone, address, vaccine, hospital, datetime, allergy, emergencyContactName, emergencyContactPhone, medicareNumber}} = this.props;
+        const { values: { firstName, lastName, email, phone, address, vaccine, hospital, datetime, allergy, emergencyContactName, emergencyContactPhone, medicareNumber } } = this.props;
 
         return (
             <MuiThemeProvider>
                 <React.Fragment>
-                    <AppBar title = "Confirm Your Details" />
+                    <AppBar title="Confirm Your Details" />
                     <List>
                         <ListItem
-                            primaryText = "First Name"
-                            secondaryText = {firstName}
+                            primaryText="First Name"
+                            secondaryText={firstName}
                         />
                         <ListItem
-                            primaryText = "Last Name"
-                            secondaryText = {lastName}
+                            primaryText="Last Name"
+                            secondaryText={lastName}
                         />
                         <ListItem
-                            primaryText = "Email"
-                            secondaryText = {email}
+                            primaryText="Email"
+                            secondaryText={email}
                         />
                         <ListItem
-                            primaryText = "Phone"
-                            secondaryText = {phone}
+                            primaryText="Phone"
+                            secondaryText={phone}
                         />
                         <ListItem
-                            primaryText = "Address"
-                            secondaryText = {address}
+                            primaryText="Address"
+                            secondaryText={address}
                         />
                         <ListItem
-                            primaryText = "Vaccine"
-                            secondaryText = {vaccine}
+                            primaryText="Vaccine"
+                            secondaryText={vaccine}
                         />
                         <ListItem
-                            primaryText = "Hospital"
-                            secondaryText = {hospital}
+                            primaryText="Hospital"
+                            secondaryText={hospital}
                         />
                         <ListItem
-                            primaryText = "Appointment Time"
-                            secondaryText = {datetime}
+                            primaryText="Appointment Time"
+                            secondaryText={datetime}
                         />
                         <ListItem
-                            primaryText = "Allergy"
-                            secondaryText = {allergy}
+                            primaryText="Allergy"
+                            secondaryText={allergy}
                         />
                         <ListItem
-                            primaryText = "Emergency Contact (Name)"
-                            secondaryText = {emergencyContactName}
+                            primaryText="Emergency Contact (Name)"
+                            secondaryText={emergencyContactName}
                         />
                         <ListItem
-                            primaryText = "Emergency Contact (Phone)"
-                            secondaryText = {emergencyContactPhone}
+                            primaryText="Emergency Contact (Phone)"
+                            secondaryText={emergencyContactPhone}
                         />
                         <ListItem
-                            primaryText = "Medicare Number"
-                            secondaryText = {medicareNumber}
+                            primaryText="Medicare Number"
+                            secondaryText={medicareNumber}
                         />
                     </List>
                     <br />
                     <RaisedButton
-                        label = "Confirm"
-                        primary = {true}
-                        style = {styles.button}
-                        onClick = {this.continue}
+                        label="Confirm"
+                        primary={true}
+                        style={styles.button}
+                        onClick={this.continue}
                     />
                     <RaisedButton
-                        label = "Back"
-                        primary = {false}
-                        style = {styles.button}
-                        onClick = {this.back}
+                        label="Back"
+                        primary={false}
+                        style={styles.button}
+                        onClick={this.back}
                     />
                 </React.Fragment>
             </MuiThemeProvider>
