c0921cb7869e449fa4bc7648809c300b051502b8
Anthony <anthony.singgih@yahoo.com>
2020-05-14 17:54:42 +1000

    alt img warning

--- client/src/components/faculties/Faculties.js
+++ client/src/components/faculties/Faculties.js
@@ -8,8 +8,6 @@ function Faculties(props){
         <body className="home">
         <h2> IN COLLABORATION WITH:</h2>
         <div className="collaboration">
-            <img className="image" src={Gavi}></img>
-            <img className="image" src={OpenIdeo}></img>
         </div>
         <h4>In response to the growing threat of pandemic disease</h4>
         <h4>our team have take the challenge to provide solutions to those affected</h4>
