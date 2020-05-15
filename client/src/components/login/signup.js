c21d64754fee52f7ef8b80759928eedbf3a84aa1
Anthony <anthony.singgih@yahoo.com>
2020-05-15 21:49:33 +1000

    changes

--- client/src/components/login/signup.js
+++ client/src/components/login/signup.js
@@ -3,16 +3,22 @@ import {Link} from 'react-router-dom';
 
 class Signup extends React.Component{
 
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         this.state = {
-
+            username: "",
+            password: "",
+            firstname: "",
+            lastname: "",
+            address: "",
+            contact: "",
+            birthday: "",
+            confirm: false
         }
+        this.collect_person = this.collect_person.bind(this)
     }
 
-
-
-    _person() {
+    collect_person() {
         const Username = document.getElementById('username').value;
         const Password = document.getElementById('password').value;
         const Firstname = document.getElementById('firstname').value;
@@ -20,45 +26,66 @@ class Signup extends React.Component{
         const Address = document.getElementById('address').value;
         const Contact = document.getElementById('contact').value;
         const DoB = document.getElementById('DoB').value;
-        
+
+
+
+        this.setState({username: Username});
+        this.setState({password: Password});
+        this.setState({firstname: Firstname});
+        this.setState({lastname: Lastname})
+        this.setState({address: Address})
+        this.setState({contact: Contact})
+        this.setState({birthday: DoB})
+
+        this.setState({confirm: true})
     }
 
+
     render() {
-        return (
-            <body className="signup">
-            <h2>Create a New Account</h2>
-            <div className="input-container">
-                <input type="text" id="username" placeholder="Username or Email" required=""/>
-            </div>
-            <div className="input-container">
-                <input type="text" id="password" placeholder="Password" required=""/>
-            </div>
-            <div className="input-container">
-                <input type="text" id="firstname" placeholder="First Name" required=""/>
-            </div>
-            <div className="input-container">
-                <input type="text" id="lastname" placeholder="Last Name" required=""/>
-            </div>
-            <div className="input-container">
-                <input type="text" id="address" placeholder="Address" required=""/>
-            </div>
-            <div className="input-container">
-                <input type="text" id="contact" placeholder="Contact" required=""/>
-            </div>
-            <div className="selection">
-                <div>Gender :</div>
-                <select>
-                    <option value="Male">Male</option>
-                    <option value="Female">Female</option>
-                    <option value="Other">Other</option>
-                </select>
-                <div>
-                    <input type="text" id="DoB" placeholder="Date of Birth (DD/MM/YY)" required=""/>
+        if(this.state.confirm === false) {
+            return (
+                <body className="signup">
+                <h2>Create a New Account</h2>
+                <div className="input-container">
+                    <input type="text" id="username" placeholder="Username or Email" required=""/>
                 </div>
-            </div>
-            <button onClick={this._person}><Link className="button" to='/login'>Next</Link></button>
-            </body>
-        )
+                <div className="input-container">
+                    <input type="text" id="password" placeholder="Password" required=""/>
+                </div>
+                <div className="input-container">
+                    <input type="text" id="firstname" placeholder="First Name" required=""/>
+                </div>
+                <div className="input-container">
+                    <input type="text" id="lastname" placeholder="Last Name" required=""/>
+                </div>
+                <div className="input-container">
+                    <input type="text" id="address" placeholder="Address" required=""/>
+                </div>
+                <div className="input-container">
+                    <input type="text" id="contact" placeholder="Contact" required=""/>
+                </div>
+                <div className="selection">
+                    <div>Gender :</div>
+                    <select>
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
+                        <option value="Other">Other</option>
+                    </select>
+                    <div>
+                        <input type="text" id="DoB" placeholder="Date of Birth (DD/MM/YY)" required=""/>
+                    </div>
+                </div>
+                <button onClick={this.collect_person}>Next</button>
+                </body>
+            )
+        }else{
+            return(
+                <body className="signup">
+                <h2>Confirm Your Details</h2>
+                <div>Username: {this.state.username}</div>
+                </body>
+            )
+        }
     }
 }
 
