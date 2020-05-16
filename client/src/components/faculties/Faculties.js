59d3c0b39ecfca4054862f0ba309b9c93db63ebc
Anthony <anthony.singgih@yahoo.com>
2020-05-16 18:23:32 +1000

    text

--- client/src/components/faculties/Faculties.js
+++ client/src/components/faculties/Faculties.js
@@ -7,7 +7,8 @@ function Faculties(props){
             <h2>Faculties</h2>
             <h2>CONTENT WILL STRETCH BACKGROUND TO CORRECT SIZE</h2>
             <h2>IPSUM LOREN CHANGE LATER</h2>
-            <h2>THIS IS FACILITIES PAGE</h2>
+            <h2>THIS IS A STATIC PAGE</h2>
+            <h2>DO NOT CHANGE</h2>
         </div>
     )
 }
--- client/src/components/patients/Patients.js
+++ client/src/components/patients/Patients.js
@@ -7,7 +7,8 @@ function Patients(props){
             <h2>Patients</h2>
             <h2>CONTENT WILL STRETCH BACKGROUND TO CORRECT SIZE</h2>
             <h2>IPSUM LOREN CHANGE LATER</h2>
-            <h2>THIS IS PATIENTS PAGE</h2>
+            <h2>THIS IS A STATIC PAGE</h2>
+            <h2>DO NOT CHANGE</h2>
         </div>
     )
 }
