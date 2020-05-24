d0ba2b2d8e1e18201ad24311a0ff3a8ed0fb4949
Anthony <anthony.singgih@yahoo.com>
2020-05-25 07:50:21 +1000

    patients

--- client/src/components/patients/Patients.js
+++ client/src/components/patients/Patients.js
@@ -7,16 +7,19 @@ function Patients(props){
     return(
         <div>
             <Header {...props}/>
-            <div className="white-container">
-                <h2>Faculties</h2>
-                <h2>CONTENT WILL STRETCH BACKGROUND TO CORRECT SIZE</h2>
-                <h2>THIS IS A STATIC PAGE</h2>
-                <h2>DO NOT CHANGE</h2>
-                <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h2>
-                <h2>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</h2>
-                <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h2>
-                <h2>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</h2>
-                <h2> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequa</h2>
+            <div className="white-container2">
+                <article className="patients-review">
+                    <div>img</div>
+                    <div>text</div>
+                </article>
+                <article className="patients-review2">
+                    <div>text</div>
+                    <div>img</div>
+                </article>
+                <article className="patients-review">
+                    <div>img</div>
+                    <div>text</div>
+                </article>
             </div>
             <Footer></Footer>
         </div>
