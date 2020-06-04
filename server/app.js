c93a66dc42d15bb6decf2c13850a422c16fe07bd
lehang123 <gonglehan2014@gmail.com>
2020-06-04 15:37:14 +1000

    s

--- server/app.js
+++ server/app.js
@@ -35,24 +35,13 @@ if (process.env.NODE_ENV === 'production'){
   app.set('view engine', 'pug')
 }
 
-// List of all the files that should be served as-is
-let protected = ['transformed.js', 'main.css', 'favicon.ico']
-
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 app.use('/api', apiRouter);
 
-// app.get("*", (req, res) => {
 
-//   let path = req.params['0'].substring(1)
-
-//   if (protected.includes(path)) {
-//     // Return the actual file
-//     res.sendFile(`${__dirname}/build/${path}`);
-//   } else {
-//     // Otherwise, redirect to /build/index.html
-//     res.sendFile(`${__dirname}/build/index.html`);
-//   }
-// });
 
 
 
