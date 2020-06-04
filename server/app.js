34fa322c35023d95ac229a82252bf74f74f6bfb0
lehang123 <gonglehan2014@gmail.com>
2020-06-04 14:31:04 +1000

    ，，

--- server/app.js
+++ server/app.js
@@ -29,6 +29,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 
+app.get("*", (req, res) => {
+  let url = path.join(__dirname, '../client/build', 'index.html');
+  if (!url.startsWith('/app/')) // since we're on local windows
+    url = url.substring(1);
+  res.sendFile(url);
+});
+
 if (process.env.NODE_ENV === 'production'){
   app.use(express.static('../client/build'))
 }else {
